38c60981215080318f10290ccea3ac504357f8cc
gadielhv <GHERNANDEZ@GHERNANDEZ.local>
2015-07-20 17:29:42 -0500

    Avance 1

---
+++ Proyecto_Final/AdminViewController.h
@@ -0,0 +1,13 @@
+//
+//  AdminViewController.h
+//  Proyecto_Final
+//
+//  Created by GHERNANDEZ on 17/07/15.
+//  Copyright (c) 2015 GHERNANDEZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AdminViewController : UIViewController
+
+@end
---
+++ Proyecto_Final/ViewAdmin.m
@@ -0,0 +1,37 @@
+//
+//  ViewAdmin.m
+//  Proyecto_Final
+//
+//  Created by GHERNANDEZ on 17/07/15.
+//  Copyright (c) 2015 GHERNANDEZ. All rights reserved.
+//
+
+#import "ViewAdmin.h"
+
+@interface ViewAdmin ()
+
+@end
+
+@implementation ViewAdmin
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
